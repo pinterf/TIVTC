6d0862033bb24d0353a33f68930cefae36be241d
Pinterf <pinterfer@gmail.com>
2017-04-21 19:29:44 +0200

    Decide on intrinsics+C on 32 bit build, too, faster

--- src/internal.h
+++ src/internal.h
@@ -45,8 +45,9 @@
 #else
 #define USE_INTR
 //#define ALLOW_MMX
-//#define USE_C_NO_ASM
-// USE_C_NO_ASM: inline non-simd asm in tfm.cpp, tfmyuy2.cpp and tfmyv12.cpp, maybe it's faster in tritical's optimization
+#define USE_C_NO_ASM
+// USE_C_NO_ASM: inline non-simd asm in tfm.cpp, tfmyuy2.cpp and tfmyv12.cpp
+// C seems to be faster
 #endif
 
 #endif  // __Internal_H__
