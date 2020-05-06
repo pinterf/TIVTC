89200ec01707b2997e469d7f4887bbff6c867dfd
Pinterf <pinterfer@gmail.com>
2020-05-06 18:28:22 +0200

    remove unused defines

--- src/common/internal.h
+++ src/common/internal.h
@@ -4,16 +4,7 @@
 #include "avisynth.h"
 
 // these settings control whether the included code comes from old asm or newer simd/C rewrites
-
-#ifdef _M_X64
-#define USE_INTR
-#undef ALLOW_MMX
-#define USE_C_NO_ASM
-#else
-#define USE_INTR
-//#define ALLOW_MMX
 #define USE_C_NO_ASM
 // USE_C_NO_ASM: inline non-simd asm
-#endif
 
 #endif  // __Internal_H__
